c1327739b9ac7870839825cb1e787a95afacef37
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-05 20:07:13 +0200

    done

--- UIWebKitTrue/ViewController.swift
+++ UIWebKitTrue/ViewController.swift
@@ -3,18 +3,22 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+final class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var myWebKit: WKWebView!
-    @IBOutlet weak var goBack: UIToolbar!
+    @IBOutlet weak var goBack: UIBarButtonItem!
     @IBOutlet weak var goForward: UIBarButtonItem!
     @IBOutlet weak var refresh: UIBarButtonItem!
     @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    
+    private let application = UIApplication.shared
     override func viewDidLoad() {
         super.viewDidLoad()
         urlTextField.delegate = self
+        urlTextField.text = "https://google.com"
+        myWebKit.navigationDelegate = self
+        activityIndicator.isHidden = true
         
     }
     
@@ -29,20 +33,43 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-        guard let url = URL(string: textField.text!) else {
-            return
-        }
+     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        guard let url = URL(string: textField.text!) else { return }
         let requset = URLRequest(url: url)
         self.myWebKit.load(requset)
+        print("url = ", url.port, url.user, url.password, url.scheme)
     }
     
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField == urlTextField {
             self.urlTextField.resignFirstResponder()
-            }
+        }
         return true
     }
+}
+
+extension ViewController: WKNavigationDelegate {
+    
+     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        
+        self.goForward.isEnabled = false
+        self.goBack.isEnabled = false
+        activityIndicator.isHidden = false
+
+    }
+    
+     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        application.isNetworkActivityIndicatorVisible = true
+        activityIndicator.isHidden = true
+
+        if webView.canGoBack {
+            self.goBack.isEnabled = true
+        }
+        if webView.canGoForward {
+            self.goForward.isEnabled = true
+        }
+        
+    }
+    
 }
 
