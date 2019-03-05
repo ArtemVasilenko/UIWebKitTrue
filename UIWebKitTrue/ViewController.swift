73def7ad4b9ba09d8eb5158c0faeb5a96661b2b8
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-05 19:02:52 +0200

    textField done

--- UIWebKitTrue/ViewController.swift
+++ UIWebKitTrue/ViewController.swift
@@ -1,20 +1,46 @@
-//
-//  ViewController.swift
-//  UIWebKitTrue
-//
-//  Created by Артем on 3/4/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
+
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet weak var myWebKit: WKWebView!
+    @IBOutlet weak var goBack: UIToolbar!
+    @IBOutlet weak var goForward: UIBarButtonItem!
+    @IBOutlet weak var refresh: UIBarButtonItem!
+    @IBOutlet weak var urlTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        urlTextField.delegate = self
+        
     }
+    
+    
+    @IBAction func goBackAction(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func goForwardAction(_ sender: UIBarButtonItem) {
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
 
-
+        guard let url = URL(string: textField.text!) else {
+            return
+        }
+        let requset = URLRequest(url: url)
+        self.myWebKit.load(requset)
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == urlTextField {
+            self.urlTextField.resignFirstResponder()
+            }
+        return true
+    }
 }
 
